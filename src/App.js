fab53bae11de999354e604800d7c546403995402
Meet Raval <meetraval@Meets-MacBook-Pro.local>
2022-02-21 14:14:25 -0500

    testing things out

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
+import Dice from './Components/Dice';
+import { useState } from 'react';
 
 function App() {
+  const [diceVal, setDiceVal] = useState(Math.floor(Math.random() * 6));
+  const [numDice, setNumDice] = useState(1);
+
+  const setDiceNumber = (num) => {
+    if(num > 10) {
+      setNumDice(10);
+    } else if(num < 1) {
+      setNumDice(1);
+    } else {
+      setNumDice(num);
+    }
+  }  
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>test</p>
+        <h3>Dice Roller</h3>
+        <input type="number" value={numDice} id="numDice" min="1" max="10" onChange={(v) => setDiceNumber(v.target.value)} />
+        <Dice val={diceVal} />
+        {numDice}
+        <button onClick={() => setDiceVal(Math.floor(Math.random() * 6))}>Roll Dice</button>
       </header>
     </div>
   );
